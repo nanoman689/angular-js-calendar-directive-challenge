ad787e9f7e593c8d5f11006c7d55418416bf8fc4
Jason Thomas <nanoman689@gmail.com>
2015-11-14 13:05:33 -0800

    Update - 11/14/15 - 105pm
    Got the ng-repeat to work better, but the table seems to be broken for
    some reason.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,34 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
+  .directive('calendar', function(){
+    return {
+      restrict: 'E',
+      templateUrl:'template.html',
+      scope:true,
+      link: function(scope, element, attrs){
+        scope.monthlyRange = CalendarRange.getMonthlyRange(new Date());
+        scope.weekNum = function(i){
+          return ~~(i / 7);
+        }
+      }
+    }
+});
+
+(function(){
+  console.log(CalendarRange.getMonthlyRange(new Date()));
+})();
+
+
 
 // your controller and directive code go here
+
+// Ranges of years
+
+// Given a month, get the range of correct dates. (use the current month for now)
+
+// Use ng-class for the background color of the dates.
+
+// Possibly a week number.
+
+// Use ng-repeat for filling in the dates using ng-repeat conditionals.
+
+// Call the funtion, get the JSON and then parse the JSON to poluate the calendar.
